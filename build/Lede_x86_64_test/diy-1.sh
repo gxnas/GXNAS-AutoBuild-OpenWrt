4aab0603382b112dca8fbbc351da7c459cc1d446
gxnas <52808122+gxnas@users.noreply.github.com>
2021-03-23 16:00:28 +0800

    Update diy-1.sh

--- build/Lede_x86_64_test/diy-1.sh
+++ build/Lede_x86_64_test/diy-1.sh
@@ -16,7 +16,7 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 #新版去广告插件
-svn export --force https://github.com/project-lede/openwrt-app/branches/luci18/luci-app-godproxy package/diy/luci-app-godproxy
+svn export --force https://github.com/project-lede/luci-app-godproxy package/diy/luci-app-godproxy
 
 #内网测速插件
 svn export --force https://github.com/sirpdboy/NetSpeedTest package/diy/netspeedtest
