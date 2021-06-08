5d53f113f229d105733fb94d9896bc106ec5c9f8
gxnas <52808122+gxnas@users.noreply.github.com>
2021-06-08 11:42:56 +0800

    Update diy-2.sh

--- build/Lede_x86_64_gxnas/diy-2.sh
+++ build/Lede_x86_64_gxnas/diy-2.sh
@@ -5,6 +5,12 @@
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 
+# 关闭DHCP服务
+echo "close_dhcp" > package/base-files/files/etc/closedhcp
+
+# 去掉LAN口使用内置的 IPv6 管理
+sed -i "/uci commit network/i\uci set network.lan.delegate='0'" package/lean/default-settings/files/zzz-default-settings
+
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-GXNAS'' package/lean/default-settings/files/zzz-default-settings
 
@@ -12,7 +18,8 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-GXNAS''
 sed -i "s/OpenWrt /OpenWrt Lede_x86_64_博主自用旁路由版 by GXNAS build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
